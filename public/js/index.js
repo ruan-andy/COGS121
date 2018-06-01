3033fa468a4b4c95c4f5880f24b8dc9d53a1214e
jiahao.zhao <garyzhao63@gmail.com>
2018-06-01 10:36:33 -0700

    Refactor and bug fixes; TODO: Firebase should not store duplicates

--- public/js/index.js
+++ public/js/index.js
@@ -8,11 +8,14 @@ $(document).ready(() => {
   $('#profileInfo').hide();
   $('#glogout').hide();
   $('#searchResultBox').hide();
-	$('#alertsuccess').hide();
+  $('#alertsuccess').hide();
 
+  console.log('user ' + userName);
   let prevNum = 0;
   let businessID;
-  //let resultID;
+  let rbusinessID;
+  let dbusinessID;
+  let restaurantID;
 
   function getRandInteger(min, max) {
     let num = 0;
@@ -33,69 +36,102 @@ $(document).ready(() => {
     return str;
   }
 
-  console.log('user ' + userName);
-function setCat() {
-  
-}
-
-  let rbusinessID;
-
-  // clicking on show more
-  $('#reloadButton').click(() => {
-
+  function getNewRec() {
     database.ref('users/' + userName).once('value', (snapshot) => {
-    const data = snapshot.val();
+      const data = snapshot.val();
+      console.log(userName);
+      //check if the user has liked any restaurant
+      if (!(snapshot.exists()) || userName === 'none') {
+        $('#r_recom-data').html("You have no liked restaurants. Discover new cuisine.");
+        return;
+      }
 
-    //check if the user has liked any restaurant
-    if (!(snapshot.exists())) {
-      $('#r_recom-data').html("You have no liked restaurants. Discover new cuisine.");
-      return;
-    }
+      //array of all the restaurants
+      let restArr = Object.keys(data);
+      let randCat;
 
-    //array of all the restaurants
-    let restArr = Object.keys(data);
-    let randCat;
-
-    /* Ajax request to pick a random category from a random restaurant */
-    $.ajax({
-      //pick a random restaurant from the user's list of restaurants
-      url: '/business/' + data[restArr[getRandInteger(0, restArr.length - 1)]],
-      type: 'GET',
-      dataType: 'json',
-      async: false,
-      success: (restaurant) => {
+      /* Ajax request to pick a random category from a random restaurant */
+      $.ajax({
+        //pick a random restaurant from the user's list of restaurants
+        url: '/business/' + data[restArr[getRandInteger(0, restArr.length - 1)]],
+        type: 'GET',
+        dataType: 'json',
+        async: false,
+        success: (restaurant) => {
           let catLength = restaurant.categories.length;
-          //pick a random category
-          randCat = restaurant.categories[getRandInteger(0, catLength - 1)].title;
-          console.log(randCat);
+            //pick a random category
+            randCat = restaurant.categories[getRandInteger(0, catLength - 1)].title;
+            console.log(randCat);
+          }
+        });
+
+      //Ajax request to display a random restaurant from the random category
+      $.ajax({
+        url: '/search/'+ randCat +'/San Diego, CA',
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+          console.log('ajax sucess!', data);
+          //get the first business
+          const business = data.businesses[getRandInteger(0, 10)];
+          rbusinessID = business.id;
+          $('#r_recom-data').html("");
+          $('#storeBox').show();
+          $('#r_name').html(business.name);
+          $('#r_pic').attr('src', business.image_url).attr('width', '300px');
+          $('#r_info').html('Tags: ' + getTags(business.categories));
+          $('#r_address').html((business.location.display_address).join(', '));
+          $('#r_addButton').show();
+          $('#hideButton').show();
         }
       });
+    })
+  }
 
-    //Ajax request to display a random restaurant from the random category
+  function getNewDis() {
     $.ajax({
-      url: '/search/'+ randCat +'/San Diego, CA',
+      url: '/search/food/San Diego, CA',
       type: 'GET',
       dataType: 'json',
       success: (data) => {
         console.log('ajax sucess!', data);
         //get the first business
-        const business = data.businesses[getRandInteger(0, 10)];
-        rbusinessID = business.id;
-        $('#r_recom-data').html("");
-        $('#storeBox').show();
-        $('#r_name').html(business.name);
-        $('#r_pic').attr('src', business.image_url).attr('width', '300px');
-        $('#r_info').html('Tags: ' + getTags(business.categories));
-        $('#r_address').html((business.location.display_address).join(', '));
-        $('#r_addButton').show();
-        $('#hideButton').show();
+        const business = data.businesses[getRandInteger(0, 19)];
+        dbusinessID = business.id;
+        $('#d_recom-data').html("");
+        $('#discoverBox').show();
+        $('#d_name').html(business.name);
+        $('#d_pic').attr('src', business.image_url).attr('width', '300px');
+        $('#d_info').html('Tags: ' + getTags(business.categories));
+        $('#d_address').html('Address: ' + (business.location.display_address).join(', '));
       }
     });
+  }
 
-    })
+  function setBusiness(restaurantID) {
+    var dateString = Date.now();
+    console.log(dateString);
+
+    /*Do not add the same restaurant twice
+    database.ref('users/' + userName).once('value', (snapshot) => {
+      if (snapshot.hasChild(restaurantID)) {
+        return;
+      }
+    });*/
+
+    database.ref('users/' + userName + '/' + dateString).set(restaurantID);
+    var d = new Date(dateString)
+    console.log(d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear());
+  }
+
+
+  /********** End of getters and setters functions **********/
+
+
+  // clicking on show more
+  $('#reloadButton').click(() => {
+    getNewRec();
   });
-
-  let restaurantID;
 
   // Search button for name
   $('#searchButton').click(() => {
@@ -140,81 +176,29 @@ function setCat() {
     $('#hideButton').show();
   });
 
+  /******************************/
   $('#r_hideButton').click(() => {
-    console.log('clicked!');
-    $.ajax({
-      url: '/search/BBQ/San Diego, CA',
-      type: 'GET',
-      dataType: 'json',
-      success: (data) => {
-        console.log('ajax sucess!', data);
-        //get the first business
-        const business = data.businesses[getRandInteger(0, 10)];
-        rbusinessID = business.id;
-        $('#r_recom-data').html("");
-        $('#storeBox').show();
-        $('#r_name').html(business.name);
-        $('#r_pic').attr('src', business.image_url).attr('width', '300px');
-        $('#r_info').html('Tags: ' + getTags(business.categories));
-        $('#r_address').html((business.location.display_address).join(', '));
-        $('#r_addButton').show();
-        $('#hideButton').show();
-      }
-    });
+    getNewRec();
   });
 
+
+  /******************************/
   $('#s_hideButton').click(() => {
     $('#searchResultBox').hide();
   });
 
-  let dbusinessID;
-
-  // clicking on discover
+  /******************************/
   $('#discoverButton').click(() => {
-    console.log('clicked!');
-    $.ajax({
-      url: '/search/food/San Diego, CA',
-      type: 'GET',
-      dataType: 'json',
-      success: (data) => {
-        console.log('ajax sucess!', data);
-        //get the first business
-        const business = data.businesses[getRandInteger(0, 19)];
-        dbusinessID = business.id;
-        $('#d_recom-data').html("");
-        $('#discoverBox').show();
-        $('#d_name').html(business.name);
-        $('#d_pic').attr('src', business.image_url).attr('width', '300px');
-        $('#d_info').html('Tags: ' + getTags(business.categories));
-        $('#d_address').html('Address: ' + (business.location.display_address).join(', '));
-      }
-    });
+    getNewDis();
   });
 
+  /******************************/
   $('#d_hideButton').click(() => {
-    console.log('clicked!');
-    $.ajax({
-      url: '/search/food/San Diego, CA',
-      type: 'GET',
-      dataType: 'json',
-      success: (data) => {
-        console.log('ajax sucess!', data);
-        //get the first business
-        const business = data.businesses[getRandInteger(0, 19)];
-        dbusinessID = business.id;
-        $('#d_recom-data').html("");
-        $('#discoverBox').show();
-        $('#d_name').html( business.name);
-        $('#d_pic').attr('src', business.image_url).attr('width', '300px');
-        $('#d_info').html('Tags: ' + getTags(business.categories));
-        $('#d_address').html('Address: ' + (business.location.display_address).join(', '));
-      }
-    });
+    getNewDis();
   });
 
   // adding the restaurants shown above will add it to the database
   $('#r_addButton').click(() => {
-    console.log('clicked!');
     console.log("businessID: " + rbusinessID, );
 
 		/*database.ref('users/Prasanth/count').once('value', (snapshot) => {
@@ -223,120 +207,39 @@ function setCat() {
 			count = data;
 
 		} );*/
-		var dateString = Date.now();
-		console.log(dateString);
-		database.ref('users/' + userName + '/' + dateString ).set(rbusinessID);
-
-		//This is how to get date from dateString
-		var d = new Date(dateString)
-		console.log(d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear());
+		setBusiness(rbusinessID);
 
 		$('#alertsuccess').show();
 
-    /** refresh the list **/
-    $.ajax({
-      url: '/search/'+cat[getRandInteger(0,cat.length)]+'/San Diego, CA',
-      type: 'GET',
-      dataType: 'json',
-      success: (data) => {
-        console.log('ajax sucess!', data);
-        //get the first business
-        const business = data.businesses[getRandInteger(0, 10)];
-        rbusinessID = business.id;
-        $('#r_recom-data').html("");
-        $('#storeBox').show();
-        $('#r_name').html(business.name);
-        $('#r_pic').attr('src', business.image_url).attr('width', '300px');
-        $('#r_info').html('Tags: ' + getTags(business.categories));
-        $('#r_address').html((business.location.display_address).join(', '));
-        $('#r_addButton').show();
-        $('#hideButton').show();
-      }
-    });
-
-    /*$.ajax({
-      // all URLs are relative to http://localhost:3000/
-      url: '/history',
-      type: 'POST', // <-- this is POST, not GET
-      data: {
-        //stores the business ID into the database
-        name: rbusinessID,
-      },
-      success: (data) => {
-        $('#status').html(data.message);
-      }
-    });*/
+    getNewRec();
   });
 
+  /******************************/
   $('#d_addButton').click(() => {
     console.log('clicked!');
     console.log("businessID: " + dbusinessID, );
 
-		var dateString = Date.now();
-		console.log(dateString);
-		database.ref('users/' + userName + '/' + dateString ).set(dbusinessID);
+    setBusiness(dbusinessID);
 
-		$('#alertsuccess').show();
+    $('#alertsuccess').show();
 
-    /** refresh the list **/
-    $.ajax({
-      url: '/search/food/San Diego, CA',
-      type: 'GET',
-      dataType: 'json',
-      success: (data) => {
-        console.log('ajax sucess!', data);
-        //get the first business
-        const business = data.businesses[getRandInteger(0, 19)];
-        dbusinessID = business.id;
-        $('#d_recom-data').html("");
-        $('#discoverBox').show();
-        $('#d_name').html( business.name);
-        $('#d_pic').attr('src', business.image_url).attr('width', '300px');
-        $('#d_info').html('Tags: ' + getTags(business.categories));
-        $('#d_address').html('Address: ' + (business.location.display_address).join(', '));
-      }
-    });
-
-    /*$.ajax({
-      // all URLs are relative to http://localhost:3000/
-      url: '/history',
-      type: 'POST', // <-- this is POST, not GET
-      data: {
-        //stores the business ID into the database
-        name: dbusinessID,
-      },
-      success: (data) => {
-        $('#status').html(data.message);
-      }
-    });*/
+    getNewDis();
   });
 
+  /******************************/
   $('#s_addButton').click(() => {
     console.log('clicked!');
     console.log("businessID: " + restaurantID, );
 
-		var dateString = Date.now();
-		console.log(dateString);
-		database.ref('users/' + userName + '/' + dateString ).set(restaurantID);
+    setBusiness(restaurantID);
 
-			$('#alertsuccess').show();
+    $('#alertsuccess').show();
 
-    /*$.ajax({
-      // all URLs are relative to http://localhost:3000/
-      url: '/history',
-      type: 'POST', // <-- this is POST, not GET
-      data: {
-        //stores the business ID into the database
-        name: restaurantID,
-      },
-      success: (data) => {
-        $('#status').html(data.message);
-      }
-    });*/
+    getNewDis();
+
   });
 
   //Getting user location
-
   window.onload = function() {
     var startPos;
     let currLocation = [];
@@ -354,22 +257,21 @@ function setCat() {
 
 
 
-  /** CODE FOR AUTO COMPLETE
+
+  /******************************
+   * CODE FOR AUTO COMPLETE
    *
    *
    */
-
-  function autocomplete(inp, arr) {
+   function autocomplete(inp, arr) {
     /*the autocomplete function takes two arguments,
     the text field element and an array of possible autocompleted values:*/
     let currentFocus;
-
 
     /*execute a function when someone writes in the text field:*/
     inp.addEventListener("input", function(e) {
 
       let a, b, i, val = this.value;
-
 
       /* ajax request to get the list of resturants*/
       $.ajax({
@@ -406,27 +308,27 @@ function setCat() {
 
         //if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
 
-        /*create a DIV element for each matching element:*/
-        b = document.createElement("DIV");
-        /*make the matching letters bold:*/
-        b.innerHTML = arr[i].name.substr(0, val.length);
-        b.innerHTML += arr[i].name.substr(val.length);
-        /*insert a input field that will hold the current array item's value:*/
-        b.innerHTML += "<input type='hidden' value='" + arr[i].name + "'>";
-        /*execute a function when someone clicks on the item value (DIV element):*/
-        b.addEventListener("click", function(e) {
-          /*insert the value for the autocomplete text field:*/
-          inp.value = this.getElementsByTagName("input")[0].value;
-					document.getElementById("searchButton").style.background = "green";
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = arr[i].name.substr(0, val.length);
+          b.innerHTML += arr[i].name.substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i].name + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+            /*insert the value for the autocomplete text field:*/
+            inp.value = this.getElementsByTagName("input")[0].value;
+            document.getElementById("searchButton").style.background = "green";
           //console.log("RESULT ID: " + resultID);
           /*close the list of autocompleted values,
           (or any other open lists of autocompleted values:*/
           closeAllLists();
         });
-        a.appendChild(b);
+          a.appendChild(b);
 
-      }
-    });
+        }
+      });
 
     /*execute a function presses a key on the keyboard:*/
     inp.addEventListener("keydown", function(e) {
@@ -454,12 +356,6 @@ function setCat() {
         }
       }
     }); //end of autocomplete function
-
-
-
-
-
-
 
     function addActive(x) {
       /*a function to classify an item as "active":*/
